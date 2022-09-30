d312689e54c02049c996ecc53c7b21ec0f899061
Vasin Sermsampan <dev.vasin@gmail.com>
2022-09-30 21:04:17 +0700

    feat(assets): total stak & total reward

--- packages/staking-ui/src/pages/Assets/Assets.tsx
+++ packages/staking-ui/src/pages/Assets/Assets.tsx
@@ -3,15 +3,17 @@ import "./Assets.css";
 import MyValidators from "src/components/MyValidators/MyValidators";
 import StakingHistory from "src/components/StakingHistory/StakingHistory";
 import { useBasStore } from "src/stores";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { IValidator } from "@ankr.com/bas-javascript-sdk";
 import { observer } from "mobx-react";
+import JfinCoin from "src/components/JfinCoin/JfinCoin";
 
 export interface IMyValidators {
   amount: number;
   event?: unknown;
   validatorProvider: IValidator;
   validator: string;
+  reward: number;
   staker: string;
   epoch: number;
 }
@@ -23,6 +25,8 @@ const Assets = observer(() => {
   const [myValidators, setMyValidators] = useState<IMyValidators[]>();
   const [stakingHistory, setStakingHistory] =
     useState<IMyTransactionHistory[]>();
+  const [totalReward, setTotalReward] = useState<number>(0);
+  const [totalStaked, setTotalStaked] = useState<number>(0);
   const [loading, setLoading] = useState(true);
   /* -------------------------------------------------------------------------- */
   /*                                   Methods                                  */
@@ -54,12 +58,83 @@ const Assets = observer(() => {
     }, 7000);
   }, [loading]);
 
+  useMemo(() => {
+    if (!myValidators || !myValidators.length) return;
+    setTotalReward(myValidators.reduce((p, c) => p + c.reward, 0));
+    setTotalStaked(myValidators.reduce((p, c) => p + c.amount, 0));
+  }, [myValidators]);
+
   /* -------------------------------------------------------------------------- */
   /*                                    DOMS                                    */
   /* -------------------------------------------------------------------------- */
   return (
     <div className="assets-container">
-      <div className="content-card">
+      <div
+        className=""
+        style={{
+          display: "grid",
+          columnGap: "20px",
+          gridTemplateColumns: "1fr 1fr",
+        }}
+      >
+        <div className="content-card">
+          <div className="card-title">
+            <b>
+              <span>Total Staking</span>
+            </b>
+          </div>
+          <div className="card-body">
+            <div
+              style={{
+                background: "#16191d",
+                padding: "1rem",
+                borderRadius: "10px",
+                textAlign: "center",
+                fontSize: "1.5rem",
+                fontWeight: "bold",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {totalStaked.toLocaleString(undefined, {
+                minimumFractionDigits: 3,
+                maximumFractionDigits: 3,
+              })}
+              <JfinCoin />
+            </div>
+          </div>
+        </div>
+        <div className="content-card">
+          <div className="card-title">
+            <b>
+              <span>Total Reward</span>
+            </b>
+          </div>
+          <div className="card-body">
+            <div
+              style={{
+                background: "#16191d",
+                padding: "1rem",
+                borderRadius: "10px",
+                fontSize: "1.5rem",
+                fontWeight: "bold",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {totalReward.toLocaleString(undefined, {
+                minimumFractionDigits: 3,
+                maximumFractionDigits: 3,
+              })}
+              <JfinCoin />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="content-card mt-2">
         <div className="card-title">
           <b>
             <WalletOutlined /> <span>Your Staking</span>
